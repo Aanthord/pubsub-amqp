d3b2108f0a45c8d11150d5496c3fb87adb77dfd3
Aanthord <michael.doran.808@gmail.com>
2024-08-27 08:40:31 +0200

    Update common.go

--- internal/handlers/common.go
+++ internal/handlers/common.go
@@ -3,17 +3,32 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 )
 
 type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
-func respondWithError(w http.ResponseWriter, code int, message string) {
-	respondWithJSON(w, code, ErrorResponse{Error: message})
+func respondWithError(w http.ResponseWriter, r *http.Request, code int, message string) {
+	span := opentracing.SpanFromContext(r.Context())
+	if span != nil {
+		ext.Error.Set(span, true)
+		span.SetTag("http.status_code", code)
+		span.SetTag("error.message", message)
+	}
+
+	respondWithJSON(w, r, code, ErrorResponse{Error: message})
 }
 
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+func respondWithJSON(w http.ResponseWriter, r *http.Request, code int, payload interface{}) {
+	span := opentracing.SpanFromContext(r.Context())
+	if span != nil {
+		span.SetTag("http.status_code", code)
+	}
+
 	response, _ := json.Marshal(payload)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
