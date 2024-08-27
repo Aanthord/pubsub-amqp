e6ce217669dbf2d1735a812a04b0d3f3f8954d39
Aanthord <michael.doran.808@gmail.com>
2024-08-27 09:15:35 +0200

    Update message.go

--- internal/models/message.go
+++ internal/models/message.go
@@ -1,22 +1,33 @@
 package models
 
 import (
-	"time"
+    "time"
+    "github.com/google/uuid"
 )
 
 type MessagePayload struct {
-	Sender    string                 `json:"sender"`
-	Timestamp string                 `json:"timestamp"`
-	Version   string                 `json:"version"`
-	Content   map[string]interface{} `json:"content"`
-	S3URI     string                 `json:"s3_uri,omitempty"`
+    ID        string                 `json:"id"`
+    Sender    string                 `json:"sender"`
+    Timestamp string                 `json:"timestamp"`
+    Version   string                 `json:"version"`
+    Content   map[string]interface{} `json:"content"`
+    S3URI     string                 `json:"s3_uri,omitempty"`
+    TraceID   string                 `json:"trace_id"`
+    Retries   int                    `json:"retries"`
 }
 
-func NewMessagePayload(sender string, content map[string]interface{}) *MessagePayload {
-	return &MessagePayload{
-		Sender:    sender,
-		Timestamp: time.Now().UTC().Format(time.RFC3339),
-		Version:   "1.0",
-		Content:   content,
-	}
+func NewMessagePayload(sender string, content map[string]interface{}, traceID string) *MessagePayload {
+    return &MessagePayload{
+        ID:        uuid.New().String(),
+        Sender:    sender,
+        Timestamp: time.Now().UTC().Format(time.RFC3339Nano),
+        Version:   "1.0",
+        Content:   content,
+        TraceID:   traceID,
+        Retries:   0,
+    }
+}
+
+func (m *MessagePayload) IncrementRetry() {
+    m.Retries++
 }
