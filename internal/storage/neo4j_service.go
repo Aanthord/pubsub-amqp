5b7b3ce7c61706c9905cf2f5738235f52a2d26bc
Aanthord <michael.doran.808@gmail.com>
2024-08-27 11:10:12 +0200

    Update neo4j_service.go

--- internal/storage/neo4j_service.go
+++ internal/storage/neo4j_service.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/aanthord/pubsub-amqp/internal/config"
 	"github.com/aanthord/pubsub-amqp/internal/metrics"
+	"github.com/aanthord/pubsub-amqp/internal/types"
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 	"go.uber.org/zap"
 )
@@ -20,15 +20,15 @@ type neo4jService struct {
 	logger *zap.SugaredLogger
 }
 
-func NewNeo4jService() (Neo4jService, error) {
+func NewNeo4jService(config types.ConfigProvider) (Neo4jService, error) {
 	logger, _ := zap.NewProduction()
 	sugar := logger.Sugar()
 
 	driver, err := neo4j.NewDriver(
-		config.GetEnv("NEO4J_URI", "bolt://localhost:7687"),
+		config.GetNeo4jURI(),
 		neo4j.BasicAuth(
-			config.GetEnv("NEO4J_USERNAME", "neo4j"),
-			config.GetEnv("NEO4J_PASSWORD", ""),
+			config.GetNeo4jUsername(),
+			config.GetNeo4jPassword(),
 			"",
 		),
 	)
